02ca245710974207735bfbe71e167f15f96b9783
Keeeweee <epsilonmajorquezero@gmail.com>
2022-12-02 22:56:36 +0100

    Solve day 01 part 1

---
+++ day-01/first.py
@@ -0,0 +1,12 @@
+calories = [line.rstrip('\n') for line in open('data/data.txt')]
+
+elfCalories = []
+currentElf = 0
+for row in calories:
+    if row == '':
+        elfCalories.append(currentElf)
+        currentElf = 0
+    else: currentElf += int(row)
+elfCalories.append(currentElf)
+
+print(max(elfCalories))
