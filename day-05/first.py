0a32e9f586109b39f57f00b394108fe6eac2e880
Keeeweee <epsilonmajorquezero@gmail.com>
2022-12-08 02:56:31 +0100

    Solve day 05 part 1

---
+++ day-05/first.py
@@ -0,0 +1,54 @@
+rows = [line.rstrip('\n') for line in open('data/data.txt')]
+columns = 9
+
+
+def getRowElements(row):
+    elements = []
+    for i, element in enumerate(row):
+        if i % 4 == 1:
+            elements.append(element)
+    return elements
+
+
+def placeIntoColumns(columns, newElements):
+    for index, element in enumerate(newElements):
+        if element != ' ':
+            columns[index].append(element)
+
+
+def parseMove(row: str):
+    row = row.replace("move ", '')
+    row = row.replace("from ", '')
+    row = row.replace("to ", '')
+    row = row.strip()
+    return [int(char) for char in row.split(' ')]
+
+
+def getResult(space):
+    result = ''
+    for tower in space:
+        result += tower[-1]
+    return result
+
+
+space = [[] for i in range(columns)]
+initialising = True
+for row in rows:
+    if row == '':
+        initialising = False
+        for tower in space:
+            tower.reverse()
+        continue
+
+    if initialising:
+        rowElements = getRowElements(row)
+        if rowElements[0] != '1':
+            placeIntoColumns(space, rowElements)
+
+    else:
+        count, start, end = parseMove(row)
+        for i in range(count):
+            moveElement = space[start - 1].pop()
+            space[end - 1].append(moveElement)
+
+print(getResult(space))
